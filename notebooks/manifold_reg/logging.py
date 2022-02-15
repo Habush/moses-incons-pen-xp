0d4f151a380f0fe557b638364a868e4d4448a18b
Abdulrahman Semrie <hsamireh@gmail.com>
2022-02-15 23:02:55 +0300

    Mask already revoked token

--- notebooks/manifold_reg/logging.py
+++ notebooks/manifold_reg/logging.py
@@ -3,14 +3,15 @@ __author__ = 'Abdulrahman Semrie<hsamireh@gmail.com>'
 import requests
 
 
-tg_token = "5211852780:AAFsZT_368EssQKvVPOcS7sE8TjONSCbXas"
-tg_chat_id = "303606146"
+tg_token = "XXXXXXXXX"
+tg_chat_id = "XXXXXXXX"
 
 
 def log_msg(msg):
     print(msg)
     send_update_to_tg(msg)
 
+# small function to notify me on telegram about the status of long-running cells    
 def send_update_to_tg(msg):
     requests.post(f"https://api.telegram.org/bot{tg_token}/sendMessage",
                   params=dict(chat_id=tg_chat_id, text=msg))
